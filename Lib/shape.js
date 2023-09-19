39542922420a937f4de341c6f710a74df29e716e
Chiko repo <124736255+chikogit@users.noreply.github.com>
2023-09-19 11:38:26 -0400

    shape-file-added

---
+++ Lib/shape.js
@@ -0,0 +1,32 @@
+class Shape {
+    constructor() {
+        this.color = "";
+    }
+    setColor(colorVar) {
+        this.color = colorVar;
+    }
+    
+    render() {
+        return "";
+    }
+}
+
+class Circle extends Shape {
+    render() {
+        return `<circle cx="150" cy="115" r="80" fill="${this.color}"/>`;
+    }
+}
+
+class Triangle extends Shape {
+    render() {
+        return `<polygon points="150,18 244,182 56,182" fill="${this.color}"/>`;
+    }
+}
+
+class Square extends Shape {
+    render() {
+        return `<rect x="70" y="35" width="160" height="160" fill="${this.color}"/>`;
+    }
+}
+
+module.exports = { Circle, Triangle, Square };
